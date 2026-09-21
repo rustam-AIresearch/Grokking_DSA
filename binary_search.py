429fb73e33189df9fda9f967d624a785eede5383
Rustam Guliyev <86050915+rustam24052003@users.noreply.github.com>
2026-09-22 01:52:58 +0400

    binary search

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(list,item):
+    low = 0 
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        
+        if guess > item:
+            high = mid - 1 ### towards left side
+
+        else: 
+            low = mid + 1  ## towards  right side
+
+    return None 
+
+
+my_list = [0,2,4,6,8,10]
+
+
+print(f'Your value locate in {binary_search(my_list, 6)}')
